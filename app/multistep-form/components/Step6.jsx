5c8af495ad872d1178f83c0175f04adb38066206
qasim <muhammad.qasim@dijination.net>
2025-07-19 19:08:53 +0500

    upd: add api for step 6

--- app/multistep-form/components/Step6.jsx
+++ app/multistep-form/components/Step6.jsx
@@ -1,20 +1,86 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useStep } from "@/app/multistep-form/context/Context";
 import StepHeader from "./StepHeader";
+import { usePatient } from "@/app/multistep-form/context/PatientContext";
+import { updatePatient } from "@/app/services/paymentService";
+import toast, { Toaster } from "react-hot-toast";
+import Loader from "./LoaderOverlay";
 
 export default function Step6() {
   const { setCurrentStep } = useStep();
+  const { patientData } = usePatient();
+  const [isUpdating, setIsUpdating] = useState(false);
 
-  const [relationship, setRelationship] = useState("Child");
+  const contactRelationshipCodes = [
+    "SPOUSE",
+    "PARENT",
+    "CHILD",
+    "SIBLING",
+    "FRIEND",
+    "COUSIN",
+    "GUARDIAN",
+    "OTHER",];
+
+  const [relationship, setRelationship] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [form, setForm] = useState({
+    contactname: "",
+    contactrelationship: "",
+    contacthomephone: "",
+    contactworkphone: "",
+    contactmobilephone: "",
+  });
+  const initialized = useRef(false);
+
+  useEffect(() => {
+    if (!initialized.current && patientData) {
+      initialized.current = true; // set flag to true
+      setForm({
+        contactname: patientData.contactname || "",
+        contactrelationship: patientData.contactrelationship || "",
+        contacthomephone: patientData.contacthomephone || "",
+        contactworkphone: patientData.contactworkphone || "",
+        contactmobilephone: patientData.contactmobilephone || "",
+      });
+      setRelationship(patientData.contactrelationship || "");
+    }
+  }, [patientData]);
 
   const goToNextStep = () => {
     setCurrentStep((prev) => prev + 1);
   };
+  const handleSubmitChanges = async () => {
+    setIsUpdating(true);
+    try {
+      debugger
+      await updatePatient(patientData.patientid, {
+        ...form,
+        status: "a", // Active status
+      });
+      toast.success("Address updated successfully!");
+      setTimeout(() => {
+        goToNextStep()
+      }, 1000);
+      setIsEditing(false);
+    } catch (error) {
+      console.error("Update Error", error);
+      toast.error("Failed to update address. Please try again.");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
   return (
     <div className="w-full h-full flex flex-col py-6 items-center px-4 sm:px-10 text-center">
-      
+
+      <Toaster position="bottom-right" reverseOrder={false} />
+      {isUpdating && <Loader />}
       <StepHeader />
 
       {/* Emergency Contact Form */}
@@ -33,6 +99,10 @@ export default function Step6() {
             type="text"
             className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#0E0C69]"
             placeholder="Input field"
+            name="contactname"
+            onChange={handleChange}
+            disabled={!isEditing || isUpdating}
+            value={form.contactname}
           />
         </div>
 
@@ -46,6 +116,10 @@ export default function Step6() {
               type="text"
               className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#0E0C69]"
               placeholder="Input field"
+              name="contactmobilephone"
+              onChange={handleChange}
+              disabled={!isEditing || isUpdating}
+              value={form.contactmobilephone}
             />
           </div>
           <div className="flex-1">
@@ -54,6 +128,10 @@ export default function Step6() {
               type="text"
               className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#0E0C69]"
               placeholder="Input field"
+              name={"contacthomephone"}
+              onChange={handleChange}
+              disabled={!isEditing || isUpdating}
+              value={form.contacthomephone}
             />
           </div>
         </div>
@@ -70,23 +148,50 @@ export default function Step6() {
           </label>
           <select
             value={relationship}
-            onChange={(e) => setRelationship(e.target.value)}
+            disabled={!isEditing || isUpdating}
+            onChange={(e) => {
+              const val = e.target.value;
+              setRelationship(val);
+              setForm((prev) => ({
+                ...prev,
+                contactrelationship: val,
+              }));
+            }}
             className="w-full border border-gray-300 rounded-md px-4 py-2 bg-white focus:outline-none focus:ring-2 focus:ring-[#0E0C69]"
           >
-            <option value="Child">Child</option>
-            <option value="Spouse">Spouse</option>
-            <option value="Parent">Parent</option>
+            <option value="" disabled>Select Relationship</option>
+            {contactRelationshipCodes.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
           </select>
         </div>
 
         {/* Save Button */}
         <div className="text-center">
-          <button
-            onClick={goToNextStep}
+          {/* <button
+            onClick={handleSubmitChanges}
             className="bg-[#0E0C69] text-white font-medium px-8 py-2 rounded-md hover:opacity-90 transition"
           >
             Save
-          </button>
+          </button> */}
+          {!isEditing ? (
+            <button
+              onClick={() => setIsEditing(true)}
+              className="bg-black text-white font-medium px-6 py-2 rounded-md hover:opacity-90"
+            >
+              Update
+            </button>
+          ) : (
+            <button
+              onClick={handleSubmitChanges}
+              disabled={isUpdating}
+              className="bg-black text-white font-medium px-6 py-2 rounded-md hover:opacity-90"
+            >
+              {isUpdating ? "Updating..." : "Submit Changes"}
+            </button>
+          )}
         </div>
       </div>
     </div>
